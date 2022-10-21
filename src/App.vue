d8520933e2e4a14f56e3471ed32bdc62726ccfb3
InnoSan <reau_beua@gmail.com>
2022-10-21 03:10:16 +0300

    fully working, rolfan lico

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,117 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <h1><img src="./assets/logo.png" alt="" />Emoji Finder</h1>
+    <p>Find emoji by keywords {{}}</p>
+    <input
+      type="search"
+      name="emoji-search-bar"
+      id="emoji-search-bar"
+      class="emoji-search-bar"
+      placeholder="Type in emoji..."
+      v-model="searchQuery"
+    />
+  </header>
+  <emoji-list :emoji-list="filteredEmojis"></emoji-list>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmojiList from "@/components/EmojiList";
+
+async function fetchEmojis(path) {
+  const request = new Request(path);
+  const response = await fetch(request);
+
+  return response.json();
+}
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      itemsList: null,
+      searchQuery: "",
+    };
+  },
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    EmojiList,
+  },
+  computed: {
+    filteredEmojis() {
+      if (!this.searchQuery) {
+        return this.itemsList;
+      } else {
+        return this.itemsList.filter(({ keywords }) =>
+          keywords.includes(this.searchQuery.toLowerCase())
+        );
+      }
+    },
+  },
+  mounted() {
+    fetchEmojis("https://emoji-api-app.herokuapp.com/").then(
+      (emo) => (this.itemsList = emo)
+    );
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Inter, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+header {
+  background: linear-gradient(92.33deg, #196b78 0.12%, #138496 100%);
+  padding-top: 64px;
+  box-shadow: 0 8px 35px rgba(0, 0, 0, 0.16);
+}
+
+h1 {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-weight: 700;
+  font-size: 72px;
+
+  color: #ccf8ff;
+}
+
+p {
+  font-weight: 400;
+  font-size: 21px;
+  line-height: 36px;
+  margin: 12px 0;
+  padding-bottom: 64px;
+
+  color: rgba(204, 248, 255, 0.75);
+}
+
+img {
+  width: 40px;
+  opacity: 0.8;
+  margin-right: 10px;
+}
+
+.emoji-search-bar {
+  border: 1px solid #ced4da;
+  height: 64px;
+  width: 820px;
+  border-radius: 4px;
+
+  font-size: 24px;
+  color: #abb5be;
+  padding-left: 32px;
+  margin-bottom: -20px;
 }
 </style>
---
+++ src/components/EmojiList.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="emoji-list">
+    <div
+      class="emoji-item"
+      v-for="(emojiItem, index) in emojiList"
+      :key="index"
+    >
+      <p class="emoji-icon">{{ emojiItem.symbol }}</p>
+      <p class="emoji-title">{{ emojiItem.title }}</p>
+      <p class="emoji-keywords">{{ emojiItem.keywords }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    emojiList: {
+      required: true,
+    },
+  },
+
+  name: "EmojiList",
+};
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+p {
+  color: black;
+}
+
+.emoji-icon {
+  font-weight: 400;
+  font-size: 48px;
+  margin-top: 30px;
+}
+
+.emoji-title {
+  font-weight: 700;
+  font-size: 17px;
+  margin-top: 18px;
+  line-height: 28px;
+
+  text-decoration-line: underline;
+}
+
+.emoji-keywords {
+  margin-top: 12px;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 15px;
+  text-align: center;
+
+  color: #3f444a;
+}
+
+.emoji-item {
+  width: 316px;
+  height: 185px;
+
+  margin-bottom: 25px;
+
+  border: 1px solid #dee2e6;
+  border-radius: 4px;
+  box-shadow: 0 8px 35px rgba(0, 0, 0, 0.15);
+  transition: 0.3s;
+}
+
+.emoji-item:hover {
+  box-shadow: 0 8px 35px rgba(0, 0, 0, 0.25);
+}
+
+.emoji-list {
+  margin: 68px auto 0;
+  display: flex;
+  flex-wrap: wrap;
+  width: 1000px;
+  justify-content: space-between;
+  flex-direction: row;
+}
+</style>
